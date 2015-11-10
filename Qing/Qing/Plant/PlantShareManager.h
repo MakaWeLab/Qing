c3a4fa4b1c7861f4e3780c2739283e99c57cfc07
MakaWeLab <maka@carlyhuangdeMac-mini.local>
2015-11-10 13:23:07 +0800

    commit

---
+++ Qing/Qing/Plant/PlantShareManager.h
@@ -0,0 +1,15 @@
+//
+//  PlantShareManager.h
+//  Qing
+//
+//  Created by Maka on 15/11/10.
+//  Copyright © 2015年 maka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlantShareManager : NSObject
+
++(instancetype)shareInstance;
+
+@end
