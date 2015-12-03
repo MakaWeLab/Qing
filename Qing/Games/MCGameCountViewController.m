d2a173f45f288efd2414373a1e7a96e67329ef86
MakaWeLab <maka@carlyhuangdeMac-mini.local>
2015-12-03 14:23:44 +0800

    add

--- Qing/Games/MCGameCountViewController.m
+++ Qing/Games/MCGameCountViewController.m
@@ -211,6 +211,15 @@
 {
     if (self.isPlaying) {
         [self.loadingWebView reload];
+        @weakify(self);
+        if (!self.isPersonDriver) {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(60 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                @strongify(self);
+                if (!self.isPersonDriver) {
+                    [self endPlay];
+                }
+            });
+        }
         return;
     }
     
